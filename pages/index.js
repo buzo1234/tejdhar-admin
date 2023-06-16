dff7a298a75548fa911223a189957f5e42077904
buzo1234 <karandua2002@gmail.com>
2023-06-16 16:24:51 +0530

    payment

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,8 @@ export default function Home({ products }) {
       </Head>
       <div>
         <div className='p-2'>
-          <p className='text-red-700 text-xl mt-2'>
-            ERROR<span className='text-sm'>(subscription-terminated)</span>
+          <p className='text-red-700 text-xl mt-2 font-semibold'>
+            ERROR<span className='text-sm ml-2'>(subscription-terminated)</span>
           </p>
           <p>Subscription terminated until further payment is initiated!</p>
         </div>
